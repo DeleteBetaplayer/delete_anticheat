8f8f15b2557c8626975ce4760b9bc718f63c577b
Berat Kansız <55640573+DeleteBetaplayer@users.noreply.github.com>
2025-01-05 01:04:07 +0300

    Update config.lua

--- dosyalar/config.lua
+++ dosyalar/config.lua
@@ -1,1 +1,12 @@
 
+yasakliDatalar = {
+    ["money"] = { "rapid_global", "rapid_account", "rapid_anticheat" },
+    ["bankmoney"] = { "rapid_global", "rapid_account", "rapid_anticheat", "rapid_admins" },
+    ["admin"] = { "rapid_global", "rapid_account", "rapid_anticheat", "rapid_admins" },
+}
+
+
+discordWebhookURL = "https://discord.com/api/webhooks/1323389115408060426/gsqbKlSLI-_1Bhq3aNKVZAGxlqM8fvPyNNbuQPffQZrZgGjaJHmKWANMZV9plLhXCz4E"
+lisansKey = "lisans giriniz"
+
+
