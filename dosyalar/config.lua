ee6daa164fe1d12f59013c0f9ef2bee37e0539d3
Berat Kansız <55640573+DeleteBetaplayer@users.noreply.github.com>
2025-01-06 00:01:08 +0300

    Add files via upload

--- dosyalar/config.lua
+++ dosyalar/config.lua
@@ -1,4 +1,3 @@
-
 yasakliDatalar = {
     ["money"] = { "rapid_global", "rapid_account", "rapid_anticheat" },
     ["bankmoney"] = { "rapid_global", "rapid_account", "rapid_anticheat", "rapid_admins" },
@@ -6,7 +5,14 @@ yasakliDatalar = {
 }
 
 
+silahBasmaScriptleri = {
+    ["rapid_items"] = true,
+    ["rapid_global"] = true,
+    ["rapid_admins"] = true
+}
+
 discordWebhookURL = "https://discord.com/api/webhooks/1323389115408060426/gsqbKlSLI-_1Bhq3aNKVZAGxlqM8fvPyNNbuQPffQZrZgGjaJHmKWANMZV9plLhXCz4E"
-lisansKey = "lisans giriniz"
+lisansKey = "Lisans Giriniz"
 
-
+maxSpeed = 200 -- Maksimum izin verilen hız (km/h)
+maxHeight = 10 -- Maksimum izin verilen zıplama yüksekliği
