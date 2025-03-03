309165bcdbc1d92d5b02779d4720f6d5f6781e94
Berat Kansız <55640573+DeleteBetaplayer@users.noreply.github.com>
2025-03-03 22:56:07 +0300

    güncelleme v0.3

--- dosyalar/config.lua
+++ dosyalar/config.lua
@@ -12,7 +12,33 @@ silahBasmaScriptleri = {
 }
 
 discordWebhookURL = "https://discord.com/api/webhooks/1323389115408060426/gsqbKlSLI-_1Bhq3aNKVZAGxlqM8fvPyNNbuQPffQZrZgGjaJHmKWANMZV9plLhXCz4E"
-lisansKey = "Lisans Giriniz"
+lisansKey = ""
 
 maxSpeed = 200 -- Maksimum izin verilen hız (km/h)
 maxHeight = 10 -- Maksimum izin verilen zıplama yüksekliği
+
+
+
+-- Engellenmesi gereken komutlar listesi
+KOMUT_ENGELLEME = {
+    "fuckac",
+    "admin",
+    "bazuka",
+    "killall",
+    "makttr",
+    "xvehicle",
+    "p4nel",
+    "vsploit",
+    -- Yeni komutlar eklemek için buraya yazabilirsiniz
+    "shutdown",
+    "restart"
+}
+
+
+
+-- İzin verilen serial numaralarını içeren liste
+serialList = {
+    "F1593F9ADDC1D02B4E6A94CADBE41E82",  -- Bu serial'e sahip oyuncu komut kullanabilir
+    "A29A833EE85E249D0A5F29489A683CB4",  -- Bu serial'e sahip oyuncu komut kullanabilir
+    -- Yeni serial numaralarını buraya ekleyebilirsiniz
+}
