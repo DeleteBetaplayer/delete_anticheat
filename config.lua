0a271d064d8e9df548c17758185517f143514584
Berat Kansız <55640573+DeleteBetaplayer@users.noreply.github.com>
2025-03-06 02:25:57 +0300

    V0.5
    • Animasyon hilesi eklendi
    • Nick değiştirme hilesi eklendi.

---
+++ config.lua
@@ -0,0 +1,51 @@
+yasakliDatalar = {
+    ["money"] = { "rapid_global", "rapid_account", "rapid_anticheat" },
+    ["bankmoney"] = { "rapid_global", "rapid_account", "rapid_anticheat", "rapid_admins" },
+    ["admin"] = { "rapid_global", "rapid_account", "rapid_anticheat", "rapid_admins" },
+}
+
+
+silahBasmaScriptleri = {
+    ["rapid_items"] = true,
+    ["rapid_global"] = true,
+    ["rapid_admins"] = true
+}
+
+discordWebhookURL = "https://discord.com/api/webhooks/1323389115408060426/gsqbKlSLI-_1Bhq3aNKVZAGxlqM8fvPyNNbuQPffQZrZgGjaJHmKWANMZV9plLhXCz4E"
+lisansKey = ""
+
+maxSpeed = 200 -- Maksimum izin verilen hız (km/h)
+maxHeight = 10 -- Maksimum izin verilen zıplama yüksekliği
+
+
+
+-- Engellenmesi gereken komutlar listesi
+KOMUT_ENGELLEME = {
+    "fuckac",
+    "admin",
+    "bazuka",
+    "killall",
+    "makttr",
+    "xvehicle",
+    "p4nel",
+    "vsploit",
+    -- Yeni komutlar eklemek için buraya yazabilirsiniz
+    "shutdown",
+    "restart"
+}
+
+
+
+-- İzin verilen serial numaralarını içeren liste
+serialList = {
+    "F1593F9ADDC1D02B4E6A94CADBE41E82",  -- Bu serial'e sahip oyuncu komut kullanabilir
+    "A29A833EE85E249D0A5F29489A683CB4",  -- Bu serial'e sahip oyuncu komut kullanabilir
+    -- Yeni serial numaralarını buraya ekleyebilirsiniz
+}
+
+
+nickSerialList = {
+    ["F1593F9ADDC1D02B4E6A94CADBE41E82"] = true,
+    ["SERIAL_2"] = true,
+    ["SERIAL_3"] = true
+}
