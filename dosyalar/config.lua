d086ff3a82a1c2845704a5f710589203b435ffa7
Berat Kansız <55640573+DeleteBetaplayer@users.noreply.github.com>
2025-03-07 02:10:09 +0300

    Add files via upload

--- dosyalar/config.lua
+++ dosyalar/config.lua
@@ -1,4 +1,4 @@
-yasakliDatalar = {
+yasakliDatalar = { -- ilk kısım data, ikinci kısımlar izin verilen sistemler
     ["money"] = { "rapid_global", "rapid_account", "rapid_anticheat" },
     ["bankmoney"] = { "rapid_global", "rapid_account", "rapid_anticheat", "rapid_admins" },
     ["admin"] = { "rapid_global", "rapid_account", "rapid_anticheat", "rapid_admins" },
@@ -12,14 +12,13 @@ silahBasmaScriptleri = {
 }
 
 discordWebhookURL = "https://discord.com/api/webhooks/1323389115408060426/gsqbKlSLI-_1Bhq3aNKVZAGxlqM8fvPyNNbuQPffQZrZgGjaJHmKWANMZV9plLhXCz4E"
-lisansKey = ""
+lisansKey = "-"
 
-maxSpeed = 200 -- Maksimum izin verilen hız (km/h)
-maxHeight = 10 -- Maksimum izin verilen zıplama yüksekliği
+maxSpeed = 200 
+maxHeight = 10 
 
 
 
--- Engellenmesi gereken komutlar listesi
 KOMUT_ENGELLEME = {
     "fuckac",
     "admin",
@@ -29,23 +28,25 @@ KOMUT_ENGELLEME = {
     "xvehicle",
     "p4nel",
     "vsploit",
-    -- Yeni komutlar eklemek için buraya yazabilirsiniz
     "shutdown",
     "restart"
 }
 
 
 
--- İzin verilen serial numaralarını içeren liste
+
 serialList = {
-    "F1593F9ADDC1D02B4E6A94CADBE41E82",  -- Bu serial'e sahip oyuncu komut kullanabilir
-    "A29A833EE85E249D0A5F29489A683CB4",  -- Bu serial'e sahip oyuncu komut kullanabilir
-    -- Yeni serial numaralarını buraya ekleyebilirsiniz
+    "-",  
+    "-",  
+
 }
 
 
 nickSerialList = {
-    ["F1593F9ADDC1D02B4E6A94CADBE41E82"] = true,
+    ["-"] = true,
     ["SERIAL_2"] = true,
     ["SERIAL_3"] = true
 }
+
+
+protectedGroups = {"Console", "Admin", "SuperModerator"} -- Korumalı gruplar
